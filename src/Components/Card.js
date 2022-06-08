93bab7abdcfde36c10362bd0c165a165f638ac46
Antuenvik <iantunevik@gmail.com>
2022-06-08 14:39:16 +0200

    Made boilerplate for card component

---
+++ src/Components/Card.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <div className="card">
+      <div className="card-content">
+        <h1 className="card-title">Order Summary</h1>
+        <p className="card-text">
+          You can now listen to millions of songs, audiobooks, and podcasts on
+          any device anywhere you like!
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
