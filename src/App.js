1590a2e6fe0fd3f8063159ad0466927a4454643c
00erieri <16erieri@gmail.com>
2022-06-08 15:06:08 +0200

    Built plan component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import Component4 from "./Components/Component4";
+
 function App() {
-  return <div>hej hej</div>;
+  return (
+    <div>
+      hej hej
+      <Component4 />
+    </div>
+  );
 }
 
 export default App;
